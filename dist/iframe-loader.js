d6407300ab4718008bc714015c24a7d84facd031
Frank Zhang <fzhang0723@gmail.com>
2024-09-29 18:05:07 -0700

    use page to host

--- dist/iframe-loader.js
+++ dist/iframe-loader.js
@@ -1,8 +1,7 @@
 // iframe-loader.js
 (function () {
   var iframe = document.createElement("iframe");
-  iframe.src =
-    "https://ac50-2601-645-d83-59e0-d1c5-e1-151a-553a.ngrok-free.app/widget.html";
+  iframe.src = "https://publicfrank1234.github.io/widget/dist/iframe-loader.js";
   iframe.style.width = "300px";
   iframe.style.height = "400px";
   iframe.style.position = "fixed";
